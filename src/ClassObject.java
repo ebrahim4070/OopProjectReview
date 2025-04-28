86a8e83aa4a9b4c721dfc31380fbc0d5b749fe8c
ebrahim4070 <142547734+ebrahim4070@users.noreply.github.com>
2025-04-28 14:23:53 +0600

    no

--- src/ClassObject.java
+++ src/ClassObject.java
@@ -1,4 +1,8 @@
 public class ClassObject {
-    
+    String name;
+    int age;
+    void display(){
+        System.out.println("I am "+name+"and my age is "+age);
+    }
 
 }
---
+++ src/Tushar.java
@@ -0,0 +1,8 @@
+public class Tushar {
+    public static void main(String[] args) {
+        ClassObject obj = new ClassObject();
+        obj.name="Ebrahim";
+        obj.age=22;
+        obj.display();
+    }
+}
