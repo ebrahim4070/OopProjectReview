6fb7f8e27e60fdf4fababd88ed95a96cbe177d15
ebrahim4070 <142547734+ebrahim4070@users.noreply.github.com>
2025-04-28 15:02:06 +0600

    no

---
+++ src/acccessmodifier/AccessModifier.java
@@ -0,0 +1,12 @@
+package acccessmodifier;
+
+public class AccessModifier {
+    private int age;
+    public void setage(int Age) {
+        age = Age;
+    }
+
+    public int getName() {
+        return age;
+    }
+}
---
+++ src/acccessmodifier/Ebrahim1.java
@@ -0,0 +1,11 @@
+package acccessmodifier;
+
+public class Ebrahim1 {
+    public static void main(String[] args) {
+        AccessModifier obj = new AccessModifier();
+        obj.setage(22);
+        System.out.println("I am Ebrahim and my age is:");
+        System.out.print(obj.getName());
+
+    }
+}
